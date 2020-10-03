966fd15ee7f62df5291e940928e180febc4ee747
Stas <stanislav_krikunov@mail.ru>
2020-10-03 17:05:26 +0300

    tests for True/False were added

--- tests.py
+++ tests.py
@@ -13,6 +13,7 @@ def test_set():
     key = 'a'
     val = st.get(key)
     val1 = 10
+    assert st.set(key,val1) == True, "Key value wasn't set"
     st.set(key,val1)
     res = st.get(key)
     assert res == val1, "Value for the key {} is not equal to expected".format(key)
@@ -20,6 +21,7 @@ def test_set():
     #text as value
     key = 'b'
     val = 'test'
+    assert st.set(key,val) == True, "Key value wasn't set"
     st.set(key,val)
     res = st.get(key)
     assert res == val, "Value for the key {} is not equal to expected".format(key)
@@ -28,12 +30,14 @@ def test_set():
     key = 'c'
     val = st.get(key)
     res = st.set(key,val)
+    assert res == False, "Key value wasn't set"
     assert val is None, "Value for an unexisting key is not None" 
     
     #numerical key
     key = 4
     val = st.get(key)
     res = st.set(key,val)
+    assert res == False, "Key value wasn't set"
     assert val is None, "Value for an unexisting key is not None" 
 
 def test_get():
